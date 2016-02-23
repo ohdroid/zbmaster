45cb9a70544eb2e72dd106fe604ca1ae04475d7d
ohdroid <hetianwa@gmail.com>
2016-02-24 00:10:36 +0800

    全局-添加bmob框架，与kotlin插件

---
+++ app/src/main/java/com/ohdroid/zbmaster/base/view/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.ohdroid.zbmaster.base.view;
+
+/**
+ * Created by ohdroid on 2016/2/23.
+ * 我是把activity与fragment规划在mvp中view层
+ * 所以base activity 主要作用是实现BaseView的功能，以及绑定presenter等通用操作
+ */
+public abstract class BaseActivity implements BaseView {
+
+}
